761e8fdeb59faf972e102f5139ce6d26a99b0adc
AdnRamos <Aderamo108@gmail.com>
2024-08-19 13:42:26 -0300

    fix: correções do build para deploy

--- src/components/Produto/NovoEstoque/dadosEstoque/index.jsx
+++ src/components/Produto/NovoEstoque/dadosEstoque/index.jsx
@@ -8,69 +8,69 @@ export default function DadosProduto({ formik }) {
       </div>
       <div className={style.container__ContainerForm_form_halfContainer}>
         <div>
-          <label htmlFor="estoque.batch">Lote<span>*</span></label>
+          <label htmlFor="batch">Lote<span>*</span></label>
           <input
             className={style.container__ContainerForm_form_halfContainer_input}
-            id="estoque.batch"
-            name="estoque.batch"
+            id="batch"
+            name="batch"
             placeholder="Insira o código do lote"
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
-            value={formik.values.estoque.batch}
+            value={formik.values.batch}
           />
-          {formik.touched.estoque?.batch && formik.errors.estoque?.batch ? (
-            <span className={style.form__error}>{formik.errors.estoque.batch}</span>
+          {formik.touched.batch && formik.errors.batch ? (
+            <span className={style.form__error}>{formik.errors.batch}</span>
           ) : null}
         </div>
         <div>
-          <label htmlFor="estoque.quantity">Quantidade<span>*</span></label>
+          <label htmlFor="quantity">Quantidade<span>*</span></label>
           <input
             className={style.container__ContainerForm_form_halfContainer_input}
-            id="estoque.quantity"
-            name="estoque.quantity"
+            id="quantity"
+            name="quantity"
             type="number"
             placeholder="Quantidade do produto"
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
-            value={formik.values.estoque.quantity}
+            value={formik.values.quantity}
             required
           />
-          {formik.touched.estoque?.quantity && formik.errors.estoque?.quantity ? (
-            <span className={style.form__error}>{formik.errors.estoque.quantity}</span>
+          {formik.touched.quantity && formik.errors.quantity ? (
+            <span className={style.form__error}>{formik.errors.quantity}</span>
           ) : null}
         </div>
 
         <div>
-          <label htmlFor="estoque.acquisitionDate">Data da compra <span>*</span></label>
+          <label htmlFor="acquisitionDate">Data da compra <span>*</span></label>
           <input
             className={style.container__ContainerForm_form_halfContainer_input}
-            id="estoque.acquisitionDate"
-            name="estoque.acquisitionDate"
+            id="acquisitionDate"
+            name="acquisitionDate"
             type="date"
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
-            value={formik.values.estoque.acquisitionDate}
+            value={formik.values.acquisitionDate}
             required
           />
-          {formik.touched.estoque?.acquisitionDate && formik.errors.estoque?.acquisitionDate ? (
-            <span className={style.form__error}>{formik.errors.estoque.acquisitionDate}</span>
+          {formik.touched.acquisitionDate && formik.errors.acquisitionDate ? (
+            <span className={style.form__error}>{formik.errors.acquisitionDate}</span>
           ) : null}
         </div>
 
         <div>
-          <label htmlFor="estoque.expirationDate">Data de validade <span>*</span></label>
+          <label htmlFor="expirationDate">Data de validade <span>*</span></label>
           <input
             className={style.container__ContainerForm_form_halfContainer_input}
-            id="estoque.expirationDate"
-            name="estoque.expirationDate"
+            id="expirationDate"
+            name="expirationDate"
             type="date"
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
-            value={formik.values.estoque.expirationDate}
+            value={formik.values.expirationDate}
             required
           />
-          {formik.touched.estoque?.expirationDate && formik.errors.estoque?.expirationDate ? (
-            <span className={style.form__error}>{formik.errors.estoque.expirationDate}</span>
+          {formik.touched.expirationDate && formik.errors.expirationDate ? (
+            <span className={style.form__error}>{formik.errors.expirationDate}</span>
           ) : null}
         </div>
       </div>
